a9d62cfc90b7c55fbeb6c0b7d66018902bdcdb64
frankwuzp <me@wuzhiping.top>
2024-12-03 14:57:53 +0800

    updated new logos

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -12,7 +12,7 @@ interface ISiteMetadataResult {
 const data: ISiteMetadataResult = {
   siteTitle: "Zipping's Running Page",
   siteUrl: 'https://m.wuzhiping.top/running',
-  logo: 'https://media.istockphoto.com/id/1158723576/vector/running-man-icon-sign-flat.jpg?s=612x612&w=0&k=20&c=Nfj6k5NvsAdx9nS5JeqrK_tkpVvJ1pDHZfe7mqSvMOU=',
+  logo: 'https://ooo.0x0.ooo/2023/12/20/OKzpKP.png', //'https://media.istockphoto.com/id/1158723576/vector/running-man-icon-sign-flat.jpg?s=612x612&w=0&k=20&c=Nfj6k5NvsAdx9nS5JeqrK_tkpVvJ1pDHZfe7mqSvMOU=',
   description: 'Personal site and blog',
   navLinks: [
     {
